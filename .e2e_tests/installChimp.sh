6de0ea5d67adf926338e1c9fb99e864b6f721a6f
Martin H. J. Bramwell <martinhbramwell@gmail.com>
2017-01-06 13:12:43 -0500

    Bug (install): Add workaround for NPM / PhantomJS defect.

--- .e2e_tests/installChimp.sh
+++ .e2e_tests/installChimp.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 #
+function installPhantomJS()
+{
+
+  if ! npm list -g phantomjs-prebuilt &>/dev/null; then
+
+    echo -e "### Installing PhantomJS";
+    npm install -gy phantomjs-prebuilt@2.1.12;
+
+  fi
+  echo -e "### PhantomJS Installed";
+}
+
 function installChimp()
 {
+  installPhantomJS;
+
   declare CHIMPVERSION=$(chimp --version 2>&1 >/dev/null) >/dev/null;
   if [[ "${CHIMPVERSION#*$NOCOMMAND}" != "$CHIMPVERSION" ]]; then
 
     echo -e "### Installing Chimp";
-    npm install -g chimp;
+    npm install -g chimp@0.45.1;
 
   fi
   echo -e "### Chimp Installed";

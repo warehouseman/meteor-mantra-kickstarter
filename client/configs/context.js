9260dc9dff646eeb3d8884e310af3bf74a139b27
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2017-01-08 20:47:05 -0500

    Upgrade (all): Staying up to date

--- client/configs/context.js
+++ client/configs/context.js
@@ -8,13 +8,6 @@ import {FlowRouter} from 'meteor/kadira:flow-router';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Tracker} from 'meteor/tracker';
 
-
-// import apolloClient from 'apollo-client';
-// import GQL from 'graphql-tag';
-// import { meteorClientConfig as MeteorClientConfig } from 'meteor/apollo';
-
-// const ApolloClient = new apolloClient(MeteorClientConfig());
-
 export default function () {
   return {
     Meteor,
@@ -23,9 +16,6 @@ export default function () {
     LocalState: new ReactiveDict(),
     Tracker,
     App,
-//    ApolloClient,
-//    GQL,
-//    MeteorClientConfig,
     ACL
   };
 }

6cda95ebc4b31380e8489a10b6f9917a39f1d971
Martin H. Bramwell <martinhbramwell@gmail.com>
2017-04-22 19:22:10 -0400

    Admin (versions): Updatee all versions
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- .scripts/startAcceptanceTest.sh
+++ .scripts/startAcceptanceTest.sh
@@ -11,4 +11,10 @@ ${PROJECT_ROOT}/.scripts/free.sh;
 declare CHIMP="${CHIMP_CMD:-${PROJECT_ROOT}/node_modules/.bin/chimp}";
 declare LOGS_DIR="${CIRCLE_ARTIFACTS:-/var/log/meteor}";
 
+export IDX=10;
+while printf "."; ! httping -qc1 http://localhost:3000 && ((IDX-- > 0));
+do
+  sleep 5;
+done;
+
 ${CHIMP} ${E2E}/chimp-config.js --ddp=http://localhost:3000 --path=${E2E};

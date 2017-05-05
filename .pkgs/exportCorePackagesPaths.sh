6a46ea524a1f25ff67cda796a701b16d6a03c483
Martin H. Bramwell <martinhbramwell@gmail.com>
2017-05-05 15:56:44 -0400

    Enhancement (submodule): Correct the exclusion list
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- .pkgs/exportCorePackagesPaths.sh
+++ .pkgs/exportCorePackagesPaths.sh
@@ -10,11 +10,19 @@ function exportCorePackagesPaths() {
   local PARENT_PKGS_PATH="../../.pkgs";
   local PKG_EXCLUSIONS="package_exclusions.json";
   local PKG_EXCL_PATH="${PARENT_PKGS_PATH}/${PKG_EXCLUSIONS}";
-  local EXCLUSIONS='[""]';
-
-  echo -e "### Identifying core npm packages for Meteor Mantra Kickstarter";
 
   pushd ${PKGS_DIR} >/dev/null;
+
+    echo -e "### Identifying core npm packages for Meteor Mantra Kickstarter.";
+
+    local CORE_EXCLUSIONS=$(jq -r .packages_excluded_from_core ${PKG_EXCL_PATH});
+    # echo -e "Core Excl : ${CORE_EXCLUSIONS}";
+
+    local CONTAINER_EXCLUSIONS=$(jq -r .packages_excluded_from_wrapper ${PKG_EXCL_PATH});
+    # echo -e "Wrapper Excl : ${CONTAINER_EXCLUSIONS}";
+
+    # local EXCLUSIONS='[""]';
+
 
     if [[ -f ${PKG_EXCL_PATH} ]]; then
       EXCLUSIONS=$(jq -r .packages_excluded_from_core ${PKG_EXCL_PATH});
@@ -29,16 +37,38 @@ function exportCorePackagesPaths() {
       if [[ -r ${NODE_META} && -f ${NODE_META} ]]; then
         MDL=$(cat ${NODE_META}  | jq -r .name);
         if [[ "X${MDL}X" != "XX" ]]; then
-          EXCLUDE=$(echo ${EXCLUSIONS} | jq ". | contains([\"${MDL}\"])");
-          echo -e "~~~~~~~~~~  Link '${MODULE_PATH}' (${MDL}) into project? Excluded : ${EXCLUDE} ~~~~~~~~~~~";
-          if [[ "true" != "${EXCLUDE}" ]]; then
+          # echo -e "${MDL} . . .  $(basename ${MODULE_PATH})";
+          MODULE_FILENAME=$(basename ${MODULE_PATH})
+          MODULE_CODENAME=${MODULE_FILENAME#${GITIG_PREFIX}};
+
+          EXCLUDE=true;
+          if [[  ${MODULE_FILENAME} = ${MODULE_CODENAME} ]]; then
+            # echo -e "Core : ${MODULE_CODENAME}";
+            EXCLUDE=$(echo ${CORE_EXCLUSIONS} | jq ". | contains([\"${MODULE_CODENAME}\"])");
+          else
+            # echo -e "Container : ${MODULE_CODENAME}";
+            EXCLUDE=$(echo ${CONTAINER_EXCLUSIONS} | jq ". | contains([\"${MODULE_CODENAME}\"])");
+          fi;
+
+          if [[  ${EXCLUDE} != "true"  ]]; then
             pushd ${MODULE_PATH} >/dev/null;
-              # echo "${MDL} to list ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>";
+              echo "${MDL} to list ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>";
               echo "${MDL}" >> ${LOCAL_NODEJS_PACKAGES_LIST};
               # ${METEOR_CMD} npm link;
               npm link;
             popd >/dev/null;
           fi;
+
+        #   EXCLUDE=$(echo ${EXCLUSIONS} | jq ". | contains([\"${MDL}\"])");
+        #   echo -e "~~~~~~~~~~  Link '${MODULE_PATH}' (${MDL}) into project? Excluded : ${EXCLUDE} ~~~~~~~~~~~";
+        #   if [[ "true" != "${EXCLUDE}" ]]; then
+        #     pushd ${MODULE_PATH} >/dev/null;
+        #       # echo "${MDL} to list ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>";
+        #       echo "${MDL}" >> ${LOCAL_NODEJS_PACKAGES_LIST};
+        #       # ${METEOR_CMD} npm link;
+        #       npm link;
+        #     popd >/dev/null;
+        #   fi;
         fi;
       fi;
 
@@ -51,5 +81,9 @@ function exportCorePackagesPaths() {
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  exportCorePackagesPaths ${SCRIPTPATH};
+  export LOCAL_NODEJS_PACKAGES_LIST=/dev/shm/localNodeJsPackagesList.txt;
+  echo "" > ${LOCAL_NODEJS_PACKAGES_LIST};
+  pushd meteor-mantra-kickstarter >/dev/null;
+    exportCorePackagesPaths ${SCRIPTPATH};
+  popd >/dev/null;
 fi;

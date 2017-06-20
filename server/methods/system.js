5f1b4a3445e1956a2f0b67097afeb63bbd2d4a60
Martin Bramwell <martinhbramwell@gmail.com>
2017-06-20 09:43:48 -0400

    Bug (lint)
    Signed-off-by: Martin Bramwell <martinhbramwell@gmail.com>

--- server/methods/system.js
+++ server/methods/system.js
@@ -3,8 +3,10 @@ import {Meteor} from 'meteor/meteor';
 export default function () {
   Meteor.methods({
     'system.isSubModule'() {
-      if (Meteor.settings.public.IS_GITSUBMODULE) return 'Submodule';
-       return 'Stand-alone';
+      if (Meteor.settings.public.IS_GITSUBMODULE) {
+        return 'Submodule';
+      }
+      return 'Stand-alone';
     }
   });
 }
